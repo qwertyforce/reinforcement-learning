6efb2d769227bceb5129abab4b02a84b5a9c051c
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2019-11-23 17:34:01 +0300

    a2c lunarlanderv2

--- LunarLander/a2c_MC_GAE.py
+++ LunarLander/a2c_MC_GAE.py
@@ -19,17 +19,21 @@ critic_model = tf.keras.models.Sequential([
 critic_model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(learning_rate = 0.005))
 
 
-# actor_model.load_weights('./weights/actor_model5000')
-# critic_model.load_weights('./weights/critic_model5000')
+# actor_model.load_weights('./weights_a2c/actor_model5000')
+# critic_model.load_weights('./weights_a2c/critic_model5000')
  
 
 env = gym.make('LunarLander-v2')
 env.seed(1)
-episodes = 500
+env2 = gym.make('LunarLander-v2')
+env2.seed(2)
+# env._max_episode_steps = 1000
+episodes =10000
 score=0
 episode_n=[]
-mean_score=[]
-max_score=200
+episode_n_test=[]
+score_train=[]
+score_test=[]
 
 def train2(previous_states,advantages,real_previous_values):
     actor_model.train_on_batch(previous_states, advantages)
@@ -67,7 +71,25 @@ def train(buff):
 
     train2(previous_states,advantages,real_previous_values)
 
-	
+def test():
+  score=0
+  for e in range(20):
+    state = env2.reset()
+    episode_score = 0
+    done = False
+    while not done:
+       state = state.reshape([1,8])
+       logits = actor_model(state)
+       a_dist = logits.numpy()
+       a = np.random.choice(a_dist[0],p=a_dist[0]) # Choose random action with p = action 
+       a, = np.where(a_dist[0] == a)
+       a=a[0]
+       next_state, reward, done, _ = env2.step(a)
+       episode_score +=reward
+       state=next_state
+    score+=episode_score
+  return (score/20)
+
 for e in range(episodes):
   state = env.reset()
   episode_score = 0
@@ -95,22 +117,27 @@ for e in range(episodes):
   episode_memory=np.array(episode_memory)
   train(episode_memory)
   score+=episode_score
-
+  episode_n.append(e+1)
+  score_train.append(episode_score)
   print("Episode  {}  Score  {}".format(e+1, episode_score))
   if (e+1) % 10 == 0:
-    episode_n.append(e+1)
-    mean_score.append(score/10)
-    print("Episode  mean  score  {}".format(score/10))
+    print("10 Episodes  mean train score  {}".format(score/10))
     score=0
 
   if(e+1) % 500 == 0:
-    actor_model.save_weights('./weights/actor_model'+str(e+1))
-    critic_model.save_weights('./weights/critic_model'+str(e+1))
+    test_score=test()
+    episode_n_test.append(e+1)
+    score_test.append(test_score)
+
+    actor_model.save_weights('./weights_a2c/actor_model'+str(e+1))
+    critic_model.save_weights('./weights_a2c/critic_model'+str(e+1))
     
 
 fig, ax = plt.subplots()
-ax.plot(episode_n, mean_score)
+ax.plot(episode_n, score_train)
+ax.plot(episode_n_test, score_test)
 ax.set(xlabel='episode n', ylabel='score',title=':(')
 ax.grid()
+fig.legend(['Train score', 'Test score'], loc='upper left')
 fig.savefig("a2c_MC_gae.png")
 plt.show()
